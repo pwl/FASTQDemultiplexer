885b94c4955c0dc454d68d1dcaa9b734396423e2
Paweł Biernat <pawel.biernat+github@gmail.com>
2017-05-05 11:45:03 +0200

    Added a prototype for a protocol config file

--- src/FASTQDemultiplexer.jl
+++ src/FASTQDemultiplexer.jl
@@ -1,6 +1,11 @@
 __precompile__()
 module FASTQDemultiplexer
 
+using Bio
+using Bio.Seq
+using Bio.Seq.FASTQ
+
+include("protocol.jl")
 include("base.jl")
 
 end # module
---
+++ src/protocol.jl
@@ -0,0 +1,77 @@
+using YAML
+using Iterators
+
+supported_protocols()=["marsseq"]
+
+
+immutable Interpreter{N}
+    insertread::Int
+    insertpos::UnitRange{Int}
+    cellpos::NTuple{N,UnitRange{Int}}
+    umipos::NTuple{N,UnitRange{Int}}
+    cellbarcodes::Vector{DNASequence}
+    umibarcodes::Vector{DNASequence}
+end
+
+
+function Interpreter(yamlfile::String)
+    config = YAML.load_file(yamlfile)
+    try
+        nreads = length(config["reads"])
+
+        cellpos = Array(UnitRange{Int}, nreads)
+        umipos  = Array(UnitRange{Int}, nreads)
+        insertpos = 1:0
+        insertread = 0
+        insertdefined = false
+
+        # parse the reads
+        for (i,read) in enumerate(config["reads"])
+
+            if haskey(read,"cell")
+                # TODO add safer parsing?
+                cellpos[i] = eval(parse(read["cell"]))
+            else
+                cellpos[i] = 1:0
+            end
+
+            if haskey(read,"umi")
+                umipos[i] = eval(parse(read["umi"]))
+            else
+                umipos[i] = 1:0
+            end
+
+            if haskey(read,"insert")
+                if insertdefined
+                    error("The field insert can be defined in only one read")
+                end
+                insertpos = eval(parse(read["insert"]))
+                insertread = i
+                insertdefined = true
+            end
+
+        end
+
+        barcodes = config["barcodes"]
+        # parse the barcodes
+        cellbarcodes = DNASequence[]
+        if haskey(barcodes,"cells")
+            cellbarcodes = DNASequence[bc for bc in barcodes["cells"]]
+        end
+
+        umibarcodes = DNASequence[]
+        if haskey(barcodes,"umis")
+            umibarcodes = DNASequence[bc for bc in barcodes["umis"]]
+        end
+
+        return Interpreter{nreads}(insertread,insertpos,
+                                   (cellpos...),
+                                   (umipos...),
+                                   cellbarcodes,
+                                   umibarcodes)
+
+    catch err
+        info("Unable to parse the YAML file.")
+        throw(err)
+    end
+end
